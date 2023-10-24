2bdede2039f784f127725328a6423dfe7d6260f3
creepermystery <52111693+creepermystery@users.noreply.github.com>
2023-10-24 17:36:01 +0200

    Still not working but advancing

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,35 @@
+fn print_labrinth(labrinth: Vec<Vec<u8>>, labrinth_size: usize) {
+
+    for k in 0..labrinth_size {
+        for j in 0..labrinth_size {
+            print!("{}", labrinth[j][k]);
+            print!(" ");
+        }
+    println!("");
+    }
+}
+
 fn main() {
+
+    const LABRINTH_SIZE: usize = 11; // MUST be odd and greater than 4 for the program to function correctly
+    const INTERNAL_SIZE: usize = LABRINTH_SIZE-2;
+
+    let mut labrinth: Vec<Vec<u8>> = Vec::new();
+    let end_collums: Vec<u8> = Vec::from([1u8; LABRINTH_SIZE]);
+
+    labrinth.push(end_collums.clone());
+
+    for i in 0..INTERNAL_SIZE {
+        let mut collum: Vec<u8> = Vec::new();
+        if i%2 == 0 {
+            collum.push(1);
+        }
+        else {
+            collum.push(0);
+        }
+        labrinth.push(collum);
+    }
+    labrinth.push(end_collums);
+
+    print_labrinth(labrinth, LABRINTH_SIZE);
 }
