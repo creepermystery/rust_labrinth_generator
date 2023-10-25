8789fd5a4c79d96d55a9213a10d627f55e4b7f61
creepermystery <52111693+creepermystery@users.noreply.github.com>
2023-10-25 22:54:20 +0200

    Labrinth generator and printer works

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,47 @@
 fn main() {
 
     fn print_labrinth(labrinth: Vec<Vec<u8>>, labrinth_size: usize) {
-        for k in 0..labrinth_size-1 {
-            for j in 0..labrinth_size-1 {
-                print!("{}", labrinth[j][k]);
-                print!(" ");
+        for k in 0..labrinth_size {
+            for j in 0..labrinth_size {
+                if labrinth[j][k] == 1 {
+                    print!("█");
+                }
+                else {
+                    print!(" ")
+                }
             }
             println!("");
-        }
-
-        println!("{}", labrinth[0][0]);        
+        }   
     }
     
     const LABRINTH_SIZE: usize = 11; // MUST be odd and greater than 4 for the program to function correctly
     const INTERNAL_SIZE: usize = LABRINTH_SIZE-2;
 
     let mut labrinth: Vec<Vec<u8>> = Vec::new();
-    let end_collums: Vec<u8> = Vec::from([1u8; LABRINTH_SIZE]);
+    let full_collum: Vec<u8> = Vec::from([1u8; LABRINTH_SIZE]);
 
-    labrinth.push(end_collums.clone());
+    labrinth.push(full_collum.clone());
 
     for i in 0..INTERNAL_SIZE {
         let mut collum: Vec<u8> = Vec::new();
-        if i%2 == 0 {
-            collum.push(1);
+        if i%2 == 1 {
+            labrinth.push(full_collum.clone());
         }
         else {
-            collum.push(0);
+            for l in 0..LABRINTH_SIZE {
+                if l%2 == 0 {
+                    collum.push(1);
+                }
+                else {
+                    collum.push(0);
+                }
+            }
+            labrinth.push(collum);
         }
-        labrinth.push(collum);
     }
-    labrinth.push(end_collums);
+    labrinth.push(full_collum);
 
     print_labrinth(labrinth, LABRINTH_SIZE);
+
+    
 }
